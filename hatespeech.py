6b40d5f1ff900a7dcc63099286f932eaaf768d20
Leite <joaoaugustobr@hotmail.com>
2022-02-10 17:14:36 -0300

    adding crawler script and requirements

---
+++ hatespeech.py
@@ -0,0 +1,33 @@
+import scrapy
+from html_text import extract_text
+import pandas as pd
+
+class HateSpeechSpider(scrapy.Spider):
+    name = "hatespeech"
+
+    def start_requests(self):
+        yield scrapy.Request(url="https://hatespeechdata.com/")
+
+    def parse(self, response):
+        data = []
+        dataset_selectors = response.xpath('//h2[@id="list-of-datasets"]/following-sibling::ul')
+        title_selectors = response.xpath('//h2[@id="list-of-datasets"]/following-sibling::h4')
+        for title, dataset in zip(title_selectors, dataset_selectors):
+            row = {}
+
+            title_text = title.css("::text").get()
+            row["title"] = title_text
+
+            line_selectors = dataset.css("li").getall()
+            if len(line_selectors) == 11:  # other sizes mean its not a dataset table
+                for line in line_selectors:
+                    text = extract_text(line)
+                    field, content = text.split(":", 1)
+                    field = "_".join(field.split())
+                    row[field] = content
+
+                data.append(row)
+    
+        df = pd.DataFrame(data)
+        df.to_csv("hatespeech.csv", index=False)
+    
